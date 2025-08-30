5f4f7eb22570ea24620446ce7dc46fa7a6e232f2
azizozs2000-cell <azizozs2000@gmail.com>
2025-08-30 13:33:28 +0200

    Add missing Eye import to Users component

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Badge } from '@/components/ui/badge'
 import { Plus, Search, Edit, Trash2, User, Mail, Calendar } from 'lucide-react'
+import { Eye } from 'lucide-react'
 
 const mockUsers = [
   {
